e73bf94e622ef5a6d29530ae872aaa1c8575e7ca
Izhaki <roey@gefri.org>
2019-09-13 21:14:09 +0100

    [dev] publish

--- scripts/frank-dist.js
+++ scripts/frank-dist.js
@@ -1,35 +1,51 @@
 /* eslint-disable no-console */
-const path = require('path');
-const fse = require('fs-extra');
+const { resolve, join, basename } = require('path');
+const { readFile, writeFile, copy } = require('fs-extra');
 
 const packagePath = process.cwd();
-const distPath = path.join(packagePath, './dist');
+const distPath = join(packagePath, './dist');
+
+const writeJson = (targetPath, obj) =>
+  writeFile(targetPath, JSON.stringify(obj, null, 2), 'utf8');
 
 async function createPackageFile() {
-  const packageData = await fse.readFile(
-    path.resolve(packagePath, './package.json'),
+  const packageData = await readFile(
+    resolve(packagePath, './package.json'),
     'utf8'
   );
   const { scripts, devDependencies, ...packageOthers } = JSON.parse(
     packageData
   );
+  const packageName = packageOthers.name;
   const newPackageData = {
     ...packageOthers,
     private: false,
+    main: './cjs/index.js',
+    module: './esm/index.js',
   };
-  const targetPath = path.resolve(distPath, './package.json');
 
-  await fse.writeFile(
-    targetPath,
-    JSON.stringify(newPackageData, null, 2),
-    'utf8'
-  );
+  if (packageName === '@regraph/geo') {
+    delete newPackageData.main;
+    delete newPackageData.module;
+  }
+  const targetPath = resolve(distPath, './package.json');
+
+  await writeJson(targetPath, newPackageData);
   console.log(`Created package.json in ${targetPath}`);
+}
+
+async function includeFileInBuild(file) {
+  const sourcePath = resolve(packagePath, file);
+  const targetPath = resolve(distPath, basename(file));
+  await copy(sourcePath, targetPath);
+  console.log(`Copied ${sourcePath} to ${targetPath}`);
 }
 
 async function run() {
   try {
     await createPackageFile();
+    await includeFileInBuild('./README.md');
+    await includeFileInBuild('../../LICENSE');
   } catch (err) {
     console.error(err);
     process.exit(1);
