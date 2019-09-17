865c02ce79b25c5b3e3c59dd4fb13af0d296e0a8
Izhaki <roey@gefri.org>
2019-09-18 00:41:22 +0100

    [docs] Colour update

--- docs/src/theme.js
+++ docs/src/theme.js
@@ -1,11 +1,11 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import { red } from '@material-ui/core/colors';
+import { red, purple } from '@material-ui/core/colors';
 
 // Create a theme instance.
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: purple[700],
     },
     secondary: {
       main: '#19857b',
