ae3c4b12325f5e354d2ae61379aaa472f9a9afc9
Izhaki <roey@gefri.org>
2019-09-19 22:17:07 +0100

    [arrowheads] Chevy

--- packages/arrowheads/src/index.js
+++ packages/arrowheads/src/index.js
@@ -1,4 +1,5 @@
 export { default as withMarker } from './withMarker';
 
 export { default as Triangle } from './Triangle';
+export { default as Chevy } from './Chevy';
 export { default as Perp } from './Perp';
