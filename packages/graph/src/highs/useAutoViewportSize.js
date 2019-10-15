60d754548ba9d6a7f6ea164df0740aff15d2e180
Izhaki <roey@gefri.org>
2019-10-15 23:49:09 +0100

    [graph] Extract useAutoViewportSize from withAutoViewportSize

---
+++ packages/graph/src/highs/useAutoViewportSize.js
@@ -0,0 +1,40 @@
+import { useMemo } from 'react';
+import { isObject } from '@regraph/core';
+import { union, getRight, getBottom, isRect } from '@regraph/geo/rect';
+
+const { values } = Object;
+const isEmpty = arr => arr.length === 0;
+const nullBox = { x: 0, y: 0, width: 0, height: 0 };
+const ensureRect = box => (isRect(box) ? box : { ...nullBox, ...box });
+
+export const getViewportBox = boxes => {
+  const rects = values(boxes).map(ensureRect);
+  if (isEmpty(rects)) {
+    return { width: 0, height: 0 };
+  }
+  return union(rects);
+};
+
+const getPadding = (viewportBox, padding) => {
+  if (typeof padding === 'function') {
+    return padding(viewportBox);
+  }
+  if (isObject(padding)) {
+    return padding;
+  }
+  // Same gap between top-left to top-bottom
+  const { x, y } = viewportBox;
+  return { right: x, bottom: y };
+};
+
+export default (boxes, { padding }) => {
+  const box = useMemo(() => getViewportBox(boxes), [boxes]);
+  return useMemo(() => {
+    const [width, height] = [getRight(box), getBottom(box)];
+    const { right, bottom } = getPadding(box, padding);
+    return {
+      width: width + right,
+      height: height + bottom,
+    };
+  }, [box, padding]);
+};
