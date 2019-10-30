f891b48a19dadb892d89213a2dd6ef2abb2004f4
Izhaki <roey@gefri.org>
2019-10-30 20:35:25 +0000

    [graph] Fix: html nodes are wrapped with g tag

--- packages/graph/src/Nodes.jsx
+++ packages/graph/src/Nodes.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Nodes = ({ nodes, boxes, defaults, isHtml }) => (
-  <g className="regraph-nodes">
-    {nodes.map(node => {
-      const { type, ...props } = {
-        ...node,
-        ...defaults,
-        box: boxes[node.id],
-        isHtml,
-        key: node.id,
-      };
-      return React.createElement(type, props);
-    })}
-  </g>
-);
+const Nodes = ({ nodes, boxes, defaults, isHtml }) => {
+  const getNodeElement = node => {
+    const { type, ...props } = {
+      ...node,
+      ...defaults,
+      box: boxes[node.id],
+      isHtml,
+      key: node.id,
+    };
+    return React.createElement(type, props);
+  };
+
+  const children = nodes.map(getNodeElement);
+
+  return React.createElement(
+    isHtml ? 'div' : 'g',
+    { className: 'regraph-nodes' },
+    children
+  );
+};
 
 Nodes.propTypes = {
   boxes: PropTypes.object.isRequired,
