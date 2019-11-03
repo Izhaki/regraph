b559481ad5c656c4c40932d69b34c34df69f5668
Izhaki <roey@gefri.org>
2019-11-03 23:07:32 +0000

    [graph] Remove old and unused withLayout and withAutoViewportSize

--- packages/graph/src/highs/withLayout.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import useLayout from './useLayout';
-
-export default layout => WrappedComponent => props => {
-  const wrappedProps = useLayout(layout)(props);
-  return <WrappedComponent {...wrappedProps} />;
-};
