b3ddd7e249c35f3f2b2dd66c175674ed7fa7a702
Izhaki <roey@gefri.org>
2020-01-03 22:20:58 +0000

    [chip-editor] Add keyboard shortcuts
    (Backspace for delete)

--- examples/editor/chip-editor/components/actions/DeleteSelected.jsx
+++ examples/editor/chip-editor/components/actions/DeleteSelected.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
+import { useKeyboardListener } from 'actionsack';
 import { connectDeleteAction } from '@regraph/editor';
 
-const DeleteSelected = ({ selectionEmpty, deleteSelected }) => (
-  <IconButton
-    onClick={deleteSelected}
-    disabled={selectionEmpty}
-    size="small"
-    aria-label="delete">
-    <DeleteIcon />
-  </IconButton>
-);
+const DeleteSelected = ({ selectionEmpty, deleteSelected }) => {
+  useKeyboardListener('Backspace', deleteSelected);
+  return (
+    <IconButton
+      onClick={deleteSelected}
+      disabled={selectionEmpty}
+      size="small"
+      aria-label="delete">
+      <DeleteIcon />
+    </IconButton>
+  );
+};
 
 DeleteSelected.propTypes = {
   deleteSelected: PropTypes.func,
