37905d9cd47e7117521f44bc3b54c1051df23ab8
Izhaki <roey@gefri.org>
2020-03-02 22:44:08 +0000

    Add Favicon
    Used https://realfavicongenerator.net/

--- docs/pages/_document.jsx
+++ docs/pages/_document.jsx
@@ -20,6 +20,31 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/icon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/icon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/icon/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/icon/safari-pinned-tab.svg"
+            color="#5bbad5"
+          />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
