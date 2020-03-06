f62bbb0321ef1f9d8c1c2332f0a303596202c3c6
Izhaki <roey@gefri.org>
2020-03-06 00:05:44 +0000

    [graph] Extract boxContext code from connectionLayout to useLayout hook
    A step in making connectionLayout working in editors

--- packages/graph/src/highs/useLayout.js
+++ packages/graph/src/highs/useLayout.js
@@ -1,12 +1,20 @@
 import { useMemo } from 'react';
-import update from './update';
 import { useBoxContext } from './BoxContext';
 
-export default layout =>
-  update(props => {
-    const boxContext = useBoxContext();
-    return useMemo(
-      () => layout(props, boxContext),
-      layout.deps(props) // eslint-disable-line react-hooks/exhaustive-deps
-    );
+const requestMissingBoxes = (ends = {}, boxContext) => {
+  Object.values(ends).forEach(({ id, port }) => {
+    boxContext.requestBox({ id, port });
   });
+};
+
+export default layout => props => {
+  const boxContext = useBoxContext();
+  return useMemo(
+    () => {
+      const { endsMissingBoxes, ...outProps } = layout(props);
+      requestMissingBoxes(endsMissingBoxes, boxContext);
+      return outProps;
+    },
+    layout.deps(props) // eslint-disable-line react-hooks/exhaustive-deps
+  );
+};
